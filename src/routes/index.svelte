6af4373febb9f5b4d07e6c466b73fc6ddcc703b8
Evan Flynn <erflynn21@gmail.com>
2020-12-23 15:50:49 +0800

    adds hero section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
     <title>Cumulus Creative Services</title>
 </svelte:head>
 
-<!-- <div class="relative bg-gray-50 overflow-hidden">
+<div class="relative overflow-hidden">
     <div
         class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
         aria-hidden="true">
@@ -69,192 +69,33 @@
         </div>
     </div>
 
-    <div class="relative pt-6 pb-16 sm:pb-24">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6">
-            <nav
-                class="relative flex items-center justify-between sm:h-10 md:justify-center"
-                aria-label="Global">
-                <div
-                    class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                    <div
-                        class="flex items-center justify-between w-full md:w-auto">
-                        <a href="/">
-                            <span class="sr-only">Workflow</span>
-                            <img
-                                class="h-8 w-auto sm:h-10"
-                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                alt="" />
-                        </a>
-                        <div class="-mr-2 flex items-center md:hidden">
-                            <button
-                                type="button"
-                                class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                id="main-menu"
-                                aria-haspopup="true">
-                                <span class="sr-only">Open main menu</span> -->
-<!-- Heroicon name: menu -->
-<!-- <svg
-                                    class="h-6 w-6"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    aria-hidden="true">
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="hidden md:flex md:space-x-10">
-                    <a
-                        href="/"
-                        class="font-medium text-gray-500 hover:text-gray-900">Product</a>
-
-                    <a
-                        href="/"
-                        class="font-medium text-gray-500 hover:text-gray-900">Features</a>
-
-                    <a
-                        href="/"
-                        class="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
-
-                    <a
-                        href="/"
-                        class="font-medium text-gray-500 hover:text-gray-900">Company</a>
-                </div>
-                <div
-                    class="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-                    <span class="inline-flex rounded-md shadow">
-                        <a
-                            href="/"
-                            class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50">
-                            Log in
-                        </a>
-                    </span>
-                </div>
-            </nav>
-        </div> -->
-
-<!--
-      Mobile menu, show/hide based on menu open state.
-
-      Entering: "duration-150 ease-out"
-        From: "opacity-0 scale-95"
-        To: "opacity-100 scale-100"
-      Leaving: "duration-100 ease-in"
-        From: "opacity-100 scale-100"
-        To: "opacity-0 scale-95"
-    -->
-<!-- <div
-            class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+    <main class="mt-16 mx-auto max-w-screen-2xl px-4 sm:mt-24 pb-10 relative">
+        <div class="text-center">
+            <h1
+                class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span class="xl:inline">We build powerful</span>
+                <span class="text-primary xl:inline">web</span>
+                <span class="xl:inline">and</span>
+                <span class="text-primary xl:inline">cloud</span>
+                <span class="xl:inline">solutions</span>
+            </h1>
+            <p
+                class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                Cumulus Creative Services uses a variety of tools and techniques
+                to bring powerful solutions to your business. Whether it's a
+                website or a fully automated workflow for you and your
+                employees, we've got you covered.
+            </p>
             <div
-                class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div class="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                        <img
-                            class="h-8 w-auto"
-                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                            alt="" />
-                    </div>
-                    <div class="-mr-2">
-                        <button
-                            type="button"
-                            class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                            <span class="sr-only">Close menu</span> -->
-<!-- Heroicon name: x -->
-
-<!-- <svg
-                                class="h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true">
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <div
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="main-menu">
-                    <div class="px-2 pt-2 pb-3" role="none">
-                        <a
-                            href="/"
-                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                            role="menuitem">Product</a>
-
-                        <a
-                            href="/"
-                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                            role="menuitem">Features</a>
-
-                        <a
-                            href="/"
-                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                            role="menuitem">Marketplace</a>
-
-                        <a
-                            href="/"
-                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                            role="menuitem">Company</a>
-                    </div>
-                    <div role="none">
-                        <a
-                            href="/"
-                            class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                            role="menuitem">
-                            Log in
-                        </a>
-                    </div>
+                class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+                <div class="rounded-md shadow">
+                    <a
+                        href="contact"
+                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-seconadry md:py-4 md:text-lg md:px-10">
+                        Get in Touch
+                    </a>
                 </div>
             </div>
         </div>
-
-        <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
-            <div class="text-center">
-                <h1
-                    class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                    <span class="block xl:inline">Data to enrich your</span>
-                    <span class="block text-indigo-600 xl:inline">online
-                        business</span>
-                </h1>
-                <p
-                    class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                    occaecat fugiat aliqua.
-                </p>
-                <div
-                    class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-                    <div class="rounded-md shadow">
-                        <a
-                            href="/"
-                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                            Get started
-                        </a>
-                    </div>
-                    <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                        <a
-                            href="/"
-                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
-                            Live demo
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </div>
-</div> -->
-<style>
-</style>
+    </main>
+</div>
