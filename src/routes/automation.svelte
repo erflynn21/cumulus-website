4c9d07856fc43b14bd3d2fd4e273329a1e0d64cc
Evan Flynn <erflynn21@gmail.com>
2020-12-29 15:37:57 +0800

    adds automation page content

--- src/routes/automation.svelte
+++ src/routes/automation.svelte
@@ -1,1 +1,302 @@
-<h1>Automation</h1>
+<section name="hero">
+    <div class="relative bg-white overflow-hidden mb-10">
+        <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+            <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+                <div
+                    class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+                    <h1
+                        class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+                        <span class="xl:inline">Powerful</span>
+                        <span class="text-primary xl:inline">automation
+                            workflows</span>
+                        <span class="xl:inline">in the</span>
+                        <span class="text-primary xl:inline">cloud</span>
+                    </h1>
+                    <p
+                        class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                        We design and build custom automation workflows based on
+                        your business needs and leveraging cloud technologies.
+                    </p>
+                    <div
+                        class="mt-10 sm:flex sm:justify-center lg:justify-start">
+                        <div class="rounded-md shadow">
+                            <a
+                                href="contact"
+                                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-secondary md:py-4 md:text-lg md:px-10">
+                                Request a quote
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="mt-12 relative sm:max-w-xl sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                    <div
+                        class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-xl">
+                        <img
+                            class="w-full rounded-lg hero-image"
+                            src="https://res.cloudinary.com/cumulus-creative-services/image/upload/v1609225823/anthony-choren-4C4jcOLoSpc-unsplash_vul32u.webp"
+                            alt="" />
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</section>
+
+<section name="features">
+    <div class="bg-white">
+        <div
+            class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+            <div>
+                <h2
+                    class="text-base font-semibold text-primary uppercase tracking-wide">
+                    Everything you need
+                </h2>
+                <p class="mt-2 text-3xl font-extrabold text-gray-900">
+                    Services to Automate Your Business
+                </p>
+            </div>
+            <div class="mt-12 lg:mt-0 lg:col-span-2">
+                <dl
+                    class="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-3 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
+                    <div class="flex">
+                        <!-- Heroicon name: check -->
+                        <svg
+                            class="flex-shrink-0 h-6 w-6 text-secondary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7" />
+                        </svg>
+                        <div class="ml-3">
+                            <dt
+                                class="text-lg leading-6 font-medium text-gray-900">
+                                In-Depth Analysis
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                We'll analyze your business's needs and the ways
+                                that automation can help.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <!-- Heroicon name: check -->
+                        <svg
+                            class="flex-shrink-0 h-6 w-6 text-secondary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7" />
+                        </svg>
+                        <div class="ml-3">
+                            <dt
+                                class="text-lg leading-6 font-medium text-gray-900">
+                                Integration
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                We can integrate with a vast amount of services,
+                                from Google to Dropbox to Slack.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <!-- Heroicon name: check -->
+                        <svg
+                            class="flex-shrink-0 h-6 w-6 text-secondary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7" />
+                        </svg>
+                        <div class="ml-3">
+                            <dt
+                                class="text-lg leading-6 font-medium text-gray-900">
+                                No-Code/Low-Code Options
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Need a solution set up that others can edit
+                                later without needing to learn programming? We
+                                can implement that.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <!-- Heroicon name: check -->
+                        <svg
+                            class="flex-shrink-0 h-6 w-6 text-secondary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7" />
+                        </svg>
+                        <div class="ml-3">
+                            <dt
+                                class="text-lg leading-6 font-medium text-gray-900">
+                                Custom Code Solutions
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Some solutions require custom code to be written
+                                to perform certain tasks.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <!-- Heroicon name: check -->
+                        <svg
+                            class="flex-shrink-0 h-6 w-6 text-secondary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7" />
+                        </svg>
+                        <div class="ml-3">
+                            <dt
+                                class="text-lg leading-6 font-medium text-gray-900">
+                                Automated Process Implementation
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                We'll help train and onboard you and your staff
+                                as automated processes are implemented in your
+                                organization.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <!-- Heroicon name: check -->
+                        <svg
+                            class="flex-shrink-0 h-6 w-6 text-secondary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7" />
+                        </svg>
+                        <div class="ml-3">
+                            <dt
+                                class="text-lg leading-6 font-medium text-gray-900">
+                                Continued Support
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                We can provide continued support and
+                                consultation for ongoing needs in your
+                                organization.
+                            </dd>
+                        </div>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section name="technologies">
+    <div class="bg-primary">
+        <div class="max-w-7xl mx-auto py-12 px-4">
+            <h2 class="text-3xl font-extrabold text-white text-center">
+                Technologies We Use to Bring Automation Solutions To Life
+            </h2>
+            <div
+                class="grid grid-cols-1 gap-x-10 gap-y-12 mt-14 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-items-center align-items-center">
+                <img
+                    height="75px"
+                    width="150px"
+                    class="h-12 w-auto object-contain opacity-50 transition duration-250 ease-in-out transform hover:scale-110 hover:opacity-100"
+                    src="https://res.cloudinary.com/cumulus-creative-services/image/upload/h_75/v1609136062/google-drive-logo_j5udd7.webp"
+                    alt="Google Drive" />
+                <img
+                    height="75px"
+                    width="150px"
+                    class="h-12 w-auto object-contain opacity-50 transition duration-250 ease-in-out transform hover:scale-110 hover:opacity-100"
+                    src="https://res.cloudinary.com/cumulus-creative-services/image/upload/h_75/v1609135539/logo-color-transparent_qv2oyl.svg"
+                    alt="Click Up" />
+                <img
+                    height="75px"
+                    width="150px"
+                    class="h-12 w-auto object-contain opacity-50 transition duration-250 ease-in-out transform hover:scale-110 hover:opacity-100"
+                    src="https://res.cloudinary.com/cumulus-creative-services/image/upload/h_75/v1609136432/zapier-logo_f92g1z.webp"
+                    alt="Zapier" />
+                <img
+                    height="75px"
+                    width="150px"
+                    class="h-12 w-auto object-contain opacity-50 transition duration-250 ease-in-out transform hover:scale-110 hover:opacity-100"
+                    src="https://res.cloudinary.com/cumulus-creative-services/image/upload/h_75/v1609136999/integromat-white_pe9las.svg"
+                    alt="Integromat" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section name="cta">
+    <div class="bg-white">
+        <div
+            class="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+            <h2
+                class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <span class="block">Want to get your business automated?</span>
+                <span class="block text-primary">Get in touch today.</span>
+            </h2>
+            <div class="mt-8 flex justify-center">
+                <div class="inline-flex rounded-md shadow">
+                    <a
+                        href="contact"
+                        class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:secondary">
+                        Request a Quote
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style>
+    img {
+        filter: brightness(0) invert(1);
+    }
+    img:hover {
+        filter: brightness(1) invert(0);
+    }
+
+    .hero-image {
+        filter: brightness(1) invert(0);
+    }
+</style>
