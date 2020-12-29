d360d318d7d4dc6d26969ed0a57edcc50646e0d6
Evan Flynn <erflynn21@gmail.com>
2020-12-29 11:04:28 +0800

    adds hero section to websites page

--- src/routes/websites.svelte
+++ src/routes/websites.svelte
@@ -1,1 +1,41 @@
-<h1>Websites</h1>
+<div class="relative bg-white overflow-hidden mb-10">
+    <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+        <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+            <div
+                class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+                <h1
+                    class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+                    <span class="block xl:inline">Websites that showcase your
+                        business</span>
+                    <span class="block text-primary xl:inline">to the world</span>
+                </h1>
+                <p
+                    class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                    We build websites that are modern, performant, responsive,
+                    and, most importantly, are designed specifically for you.
+                    Check out how our website development can bring to the table
+                    below.
+                </p>
+                <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
+                    <div class="rounded-md shadow">
+                        <a
+                            href="contact"
+                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-secondary md:py-4 md:text-lg md:px-10">
+                            Request a quote
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="mt-12 relative sm:max-w-xl sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                <div
+                    class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-xl">
+                    <img
+                        class="w-full rounded-lg"
+                        src="https://res.cloudinary.com/cumulus-creative-services/image/upload/v1609210757/nathan-da-silva-k-rKfqSm4L4-unsplash_z0pzrc.jpg"
+                        alt="" />
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
