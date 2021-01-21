80fca6bc6b497f1f9c11c80029546d746c82f9c7
Evan Flynn <erflynn21@gmail.com>
2021-01-21 09:35:18 +0800

    fixes image src

--- src/routes/technology-consulting.svelte
+++ src/routes/technology-consulting.svelte
@@ -2,10 +2,12 @@
     <title>Technology Consulting - Cumulus Creative Services</title>
     <meta
         name="description"
-        content="Analyzing and advising on the best and latest technology solutions specifically tailored to you." />
+        content="Analyzing and advising on the best and latest technology solutions specifically tailored to you."
+    />
     <meta
         name="keywords"
-        content="technology consulting, technology, IT, advising, analysis" />
+        content="technology consulting, technology, IT, advising, analysis"
+    />
 </svelte:head>
 
 <section name="hero">
@@ -13,21 +15,27 @@
         <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-20 sm:px-6 lg:mt-20">
             <div class="lg:grid lg:grid-cols-12 lg:gap-8">
                 <div
-                    class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+                    class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left"
+                >
                     <h1
-                        class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+                        class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
+                    >
                         <span class="xl:inline">Designing the right</span>
-                        <span class="text-primary xl:inline">technology strategy</span>
+                        <span class="text-primary xl:inline"
+                            >technology strategy</span
+                        >
                         <span class="xl:inline">for your</span>
                         <span class="text-primary xl:inline">business</span>
                     </h1>
                     <p
-                        class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                        class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl"
+                    >
                         We analyze your business's technology needs and create a
                         strategic plan with customs solutions for you.
                     </p>
                     <div
-                        class="mt-10 sm:flex sm:justify-center lg:justify-start">
+                        class="mt-10 sm:flex sm:justify-center lg:justify-start"
+                    >
                         <div class="rounded-md shadow">
                             <a
                                 href="contact"
@@ -38,13 +46,16 @@
                     </div>
                 </div>
                 <div
-                    class="mt-12 relative sm:max-w-xl sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                    class="mt-12 relative sm:max-w-xl sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center"
+                >
                     <div
-                        class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-xl">
+                        class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-xl"
+                    >
                         <img
                             class="w-full rounded-lg hero-image"
-                            src="https://res.cloudinary.com/cumulus-creative-services/image/upload/w_600/v1609222200/grovemade-RvPDe41lYBA-unsplash_vz3lc6.webp"
-                            alt="" />
+                            src="https://res.cloudinary.com/cumulus-creative-services/image/upload/w_600/v1609222200/Cumulus%20Website/grovemade-RvPDe41lYBA-unsplash_vz3lc6.webp"
+                            alt=""
+                        />
                     </div>
                 </div>
             </div>
@@ -58,9 +69,8 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="lg:text-center">
                 <h2
-                    class="text-base text-primary font-semibold tracking-wide uppercase">
-                    Technology Consulting
-                </h2>
+                    class="text-base text-primary font-semibold tracking-wide uppercase"
+                >Technology Consulting</h2>
                 <!-- <p
                     class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     Help Your Business
@@ -73,29 +83,32 @@
 
             <div class="mt-10">
                 <dl
-                    class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                    class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10"
+                >
                     <div class="flex">
                         <div class="flex-shrink-0">
                             <div
-                                class="flex items-center justify-center w-12 rounded-md">
+                                class="flex items-center justify-center w-12 rounded-md"
+                            >
                                 <svg
                                     class="w-6 h-6 text-primary"
                                     fill="none"
                                     stroke="currentColor"
                                     viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"><path
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    ><path
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                                        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+                                    />
                                 </svg>
                             </div>
                         </div>
                         <div class="ml-4">
                             <dt
-                                class="text-lg leading-6 font-medium text-gray-900">
-                                In-Depth Analysis
-                            </dt>
+                                class="text-lg leading-6 font-medium text-gray-900"
+                            >In-Depth Analysis</dt>
                             <dd class="mt-2 text-base text-gray-500">
                                 We'll analyze your business and specifically the
                                 areas that can be improved with various
@@ -107,25 +120,27 @@
                     <div class="flex">
                         <div class="flex-shrink-0">
                             <div
-                                class="flex items-center justify-center w-12 rounded-md">
+                                class="flex items-center justify-center w-12 rounded-md"
+                            >
                                 <svg
                                     class="w-6 h-6 text-primary"
                                     fill="none"
                                     stroke="currentColor"
                                     viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"><path
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    ><path
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+                                        d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                                    />
                                 </svg>
                             </div>
                         </div>
                         <div class="ml-4">
                             <dt
-                                class="text-lg leading-6 font-medium text-gray-900">
-                                Improve Processes
-                            </dt>
+                                class="text-lg leading-6 font-medium text-gray-900"
+                            >Improve Processes</dt>
                             <dd class="mt-2 text-base text-gray-500">
                                 We'll offer advice and detailed plans of how to
                                 use various tools and systems to improve the
@@ -137,25 +152,27 @@
                     <div class="flex">
                         <div class="flex-shrink-0">
                             <div
-                                class="flex items-center justify-center w-12 rounded-md">
+                                class="flex items-center justify-center w-12 rounded-md"
+                            >
                                 <svg
                                     class="w-6 h-6 text-primary"
                                     fill="none"
                                     stroke="currentColor"
                                     viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"><path
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    ><path
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
                                 </svg>
                             </div>
                         </div>
                         <div class="ml-4">
                             <dt
-                                class="text-lg leading-6 font-medium text-gray-900">
-                                Reduce Costs
-                            </dt>
+                                class="text-lg leading-6 font-medium text-gray-900"
+                            >Reduce Costs</dt>
                             <dd class="mt-2 text-base text-gray-500">
                                 Through improved processes and implementation of
                                 technology systems, we can cut operating costs
@@ -168,30 +185,33 @@
                     <div class="flex">
                         <div class="flex-shrink-0">
                             <div
-                                class="flex items-center justify-center w-12 rounded-md">
+                                class="flex items-center justify-center w-12 rounded-md"
+                            >
                                 <svg
                                     class="w-6 h-6 text-primary"
                                     fill="none"
                                     stroke="currentColor"
                                     viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"><path
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    ><path
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                                    />
                                     <path
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
                                 </svg>
                             </div>
                         </div>
                         <div class="ml-4">
                             <dt
-                                class="text-lg leading-6 font-medium text-gray-900">
-                                Solutions Implementation
-                            </dt>
+                                class="text-lg leading-6 font-medium text-gray-900"
+                            >Solutions Implementation</dt>
                             <dd class="mt-2 text-base text-gray-500">
                                 We'll set up technical solutions and help
                                 onboard both you and your employees. We also
@@ -209,11 +229,15 @@
 <section name="cta">
     <div class="bg-white">
         <div
-            class="max-w-full mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8 bg-primary">
+            class="max-w-full mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8 bg-primary"
+        >
             <h2
-                class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                <span class="block text-white mb-6">Ready to find the best
-                    technology solutions for your business?</span>
+                class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+            >
+                <span class="block text-white mb-6"
+                    >Ready to find the best technology solutions for your
+                    business?</span
+                >
                 <span class="block text-white">Get in touch today.</span>
             </h2>
             <div class="mt-8 flex justify-center">
